1e9972ce7dd9679891205f4aa19fde5f66fc01d1
xFarming Developer <kouljihate@gmail.com>
2026-05-16 13:48:45 +0200

    Add version number display at bottom of sidebar

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.config import Config
 from app.translations import t as t_func
 from app.utils.logging import setup_logging
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 
 bootstrap = Bootstrap()
 
